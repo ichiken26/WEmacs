492d5490253395e4a31ebdf1d77360ef017489e6
KOKAGE <62ichiken@gmail.com>
2026-04-16 01:24:12 +0900

    feat: commented out codes which dfon't use now

--- WEMacs/CapsLockToF13Remapper.cs
+++ WEMacs/CapsLockToF13Remapper.cs
@@ -1,145 +1,145 @@
-using System.Runtime.InteropServices;
+// using System.Runtime.InteropServices;
 
-namespace WEMacs;
+// namespace WEMacs;
 
-/// <summary>
-/// 旧実装。CapsLock を F13 に置き換える別案として参照用に保持。
-/// 現在の実行経路では <see cref="CapsLockKeyHook"/> を使用する。
-/// </summary>
-public sealed class CapsLockToF13Remapper : IDisposable
-{
-    private const int WhKeyboardLl = 13;
-    private const int WmKeydown = 0x0100;
-    private const int WmKeyup = 0x0101;
-    private const int WmSyskeydown = 0x0104;
-    private const int WmSyskeyup = 0x0105;
-    private const int HcAction = 0;
-    private const uint LlkhfUp = 0x80;
+// /// <summary>
+// /// 旧実装。CapsLock を F13 に置き換える別案として参照用に保持。
+// /// 現在の実行経路では <see cref="CapsLockKeyHook"/> を使用する。
+// /// </summary>
+// public sealed class CapsLockToF13Remapper : IDisposable
+// {
+//     private const int WhKeyboardLl = 13;
+//     private const int WmKeydown = 0x0100;
+//     private const int WmKeyup = 0x0101;
+//     private const int WmSyskeydown = 0x0104;
+//     private const int WmSyskeyup = 0x0105;
+//     private const int HcAction = 0;
+//     private const uint LlkhfUp = 0x80;
 
-    private const int VkCapital = 0x14;
-    private const ushort VkF13 = 0x7C;
+//     private const int VkCapital = 0x14;
+//     private const ushort VkF13 = 0x7C;
 
-    private const uint InputKeyboard = 1;
-    private const uint KeyeventfKeyup = 0x0002;
+//     private const uint InputKeyboard = 1;
+//     private const uint KeyeventfKeyup = 0x0002;
 
-    private IntPtr _hookId = IntPtr.Zero;
-    private readonly LowLevelKeyboardProc _proc;
+//     private IntPtr _hookId = IntPtr.Zero;
+//     private readonly LowLevelKeyboardProc _proc;
 
-    public CapsLockToF13Remapper()
-    {
-        _proc = HookCallback;
-    }
+//     public CapsLockToF13Remapper()
+//     {
+//         _proc = HookCallback;
+//     }
 
-    public void Install()
-    {
-        if (_hookId != IntPtr.Zero)
-            return;
+//     public void Install()
+//     {
+//         if (_hookId != IntPtr.Zero)
+//             return;
 
-        _hookId = SetWindowsHookEx(WhKeyboardLl, _proc, GetModuleHandle(null), 0);
-        if (_hookId == IntPtr.Zero)
-            throw new InvalidOperationException($"SetWindowsHookEx failed: {Marshal.GetLastWin32Error()}");
-    }
+//         _hookId = SetWindowsHookEx(WhKeyboardLl, _proc, GetModuleHandle(null), 0);
+//         if (_hookId == IntPtr.Zero)
+//             throw new InvalidOperationException($"SetWindowsHookEx failed: {Marshal.GetLastWin32Error()}");
+//     }
 
-    public void Uninstall()
-    {
-        if (_hookId == IntPtr.Zero)
-            return;
+//     public void Uninstall()
+//     {
+//         if (_hookId == IntPtr.Zero)
+//             return;
 
-        if (!UnhookWindowsHookEx(_hookId))
-            throw new InvalidOperationException($"UnhookWindowsHookEx failed: {Marshal.GetLastWin32Error()}");
+//         if (!UnhookWindowsHookEx(_hookId))
+//             throw new InvalidOperationException($"UnhookWindowsHookEx failed: {Marshal.GetLastWin32Error()}");
 
-        _hookId = IntPtr.Zero;
-    }
+//         _hookId = IntPtr.Zero;
+//     }
 
-    private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-    {
-        if (nCode == HcAction)
-        {
-            var info = Marshal.PtrToStructure<Kbdllhookstruct>(lParam);
-            if (info.VkCode == VkCapital)
-            {
-                var msg = unchecked((uint)(nint)wParam);
-                bool keyUp;
-                if (msg == WmKeyup || msg == WmSyskeyup)
-                    keyUp = true;
-                else if (msg == WmKeydown || msg == WmSyskeydown)
-                    keyUp = false;
-                else
-                    keyUp = (info.Flags & LlkhfUp) != 0;
+//     private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+//     {
+//         if (nCode == HcAction)
+//         {
+//             var info = Marshal.PtrToStructure<Kbdllhookstruct>(lParam);
+//             if (info.VkCode == VkCapital)
+//             {
+//                 var msg = unchecked((uint)(nint)wParam);
+//                 bool keyUp;
+//                 if (msg == WmKeyup || msg == WmSyskeyup)
+//                     keyUp = true;
+//                 else if (msg == WmKeydown || msg == WmSyskeydown)
+//                     keyUp = false;
+//                 else
+//                     keyUp = (info.Flags & LlkhfUp) != 0;
 
-                if (!TrySendF13(keyUp))
-                    return CallNextHookEx(_hookId, nCode, wParam, lParam);
-                return (IntPtr)1;
-            }
-        }
+//                 if (!TrySendF13(keyUp))
+//                     return CallNextHookEx(_hookId, nCode, wParam, lParam);
+//                 return (IntPtr)1;
+//             }
+//         }
 
-        return CallNextHookEx(_hookId, nCode, wParam, lParam);
-    }
+//         return CallNextHookEx(_hookId, nCode, wParam, lParam);
+//     }
 
-    private static bool TrySendF13(bool keyUp)
-    {
-        var ki = new Keybdinput
-        {
-            WVk = VkF13,
-            WScan = 0,
-            DwFlags = keyUp ? KeyeventfKeyup : 0,
-            Time = 0,
-            DwExtraInfo = UIntPtr.Zero
-        };
+//     private static bool TrySendF13(bool keyUp)
+//     {
+//         var ki = new Keybdinput
+//         {
+//             WVk = VkF13,
+//             WScan = 0,
+//             DwFlags = keyUp ? KeyeventfKeyup : 0,
+//             Time = 0,
+//             DwExtraInfo = UIntPtr.Zero
+//         };
 
-        var input = new Input { Type = InputKeyboard, Ki = ki };
-        var size = Marshal.SizeOf<Input>();
-        return SendInput(1, new[] { input }, size) == 1;
-    }
+//         var input = new Input { Type = InputKeyboard, Ki = ki };
+//         var size = Marshal.SizeOf<Input>();
+//         return SendInput(1, new[] { input }, size) == 1;
+//     }
 
-    public void Dispose()
-    {
-        Uninstall();
-        GC.SuppressFinalize(this);
-    }
+//     public void Dispose()
+//     {
+//         Uninstall();
+//         GC.SuppressFinalize(this);
+//     }
 
-    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+//     private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
-    [StructLayout(LayoutKind.Explicit)]
-    private struct Kbdllhookstruct
-    {
-        [FieldOffset(0)] public uint VkCode;
-        [FieldOffset(4)] public uint ScanCode;
-        [FieldOffset(8)] public uint Flags;
-        [FieldOffset(12)] public uint Time;
-        [FieldOffset(16)] public UIntPtr DwExtraInfo;
-    }
+//     [StructLayout(LayoutKind.Explicit)]
+//     private struct Kbdllhookstruct
+//     {
+//         [FieldOffset(0)] public uint VkCode;
+//         [FieldOffset(4)] public uint ScanCode;
+//         [FieldOffset(8)] public uint Flags;
+//         [FieldOffset(12)] public uint Time;
+//         [FieldOffset(16)] public UIntPtr DwExtraInfo;
+//     }
 
-    [StructLayout(LayoutKind.Sequential)]
-    private struct Input
-    {
-        public uint Type;
-        public Keybdinput Ki;
-    }
+//     [StructLayout(LayoutKind.Sequential)]
+//     private struct Input
+//     {
+//         public uint Type;
+//         public Keybdinput Ki;
+//     }
 
-    [StructLayout(LayoutKind.Sequential)]
-    private struct Keybdinput
-    {
-        public ushort WVk;
-        public ushort WScan;
-        public uint DwFlags;
-        public uint Time;
-        public UIntPtr DwExtraInfo;
-    }
+//     [StructLayout(LayoutKind.Sequential)]
+//     private struct Keybdinput
+//     {
+//         public ushort WVk;
+//         public ushort WScan;
+//         public uint DwFlags;
+//         public uint Time;
+//         public UIntPtr DwExtraInfo;
+//     }
 
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+//     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+//     private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+//     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+//     [return: MarshalAs(UnmanagedType.Bool)]
+//     private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+//     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+//     private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+//     [DllImport("user32.dll", SetLastError = true)]
+//     private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
 
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr GetModuleHandle(string? lpModuleName);
-}
+//     [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+//     private static extern IntPtr GetModuleHandle(string? lpModuleName);
+// }
